aaff82b8d272942f21332d712e563cb6a85100b5
24b01a1216 <24b01a1216@svecw.edu.in>
2026-04-03 16:59:43 +0530

    deploying

--- blogproject/blogproject/settings.py
+++ blogproject/blogproject/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from email.policy import default
 import os
 from pathlib import Path
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -76,7 +77,7 @@ WSGI_APPLICATION = 'blogproject.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-import dj_database_url
+
 DATABASES = {
     'default': {
         # 'ENGINE': 'django.db.backends.sqlite3',
